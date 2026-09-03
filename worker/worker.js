cf8c5074cab0e146317cb90a1552d13ce5f03f4e
tcisolutions <tcmcorreo@gmail.com>
2026-09-02 21:43:55 -0600

    Add files via upload

--- worker/worker.js
+++ worker/worker.js
@@ -1,1 +1,11 @@
-// Cloudflare Worker base
+
+export default {
+ async fetch(request){
+   return new Response(JSON.stringify({
+     status:"ok",
+     message:"Worker listo para conectar con GitHub API."
+   }),{
+     headers:{"content-type":"application/json"}
+   });
+ }
+}
